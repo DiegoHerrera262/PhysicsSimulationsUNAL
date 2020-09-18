6f6a210be2cb75390faa11da2504c4f63666f0d0
DiegoHerrera262 <diegoherrera262@gmail.com>
2020-09-18 12:44:45 -0500

    Compile using g++ DETest.cpp DiscreteElements.cpp

--- ClassWork/DETest.cpp
+++ ClassWork/DETest.cpp
@@ -12,30 +12,33 @@ int main(void){
   // Period for circular orbits: T = 2*pi*r0/v0
 
   // Definition of parameters for 2 planet system
-  double m0 = 16.0;
-  double r0 = 1.0;
-  double v0 = 2.0;
+  double m0 = 16.0; double m1 = 2*4.0;
+  double r0 = 1.0; double r1 = -m0/m1 * r0;
+  double v0 = std::sqrt(m1*m1 * std::pow((m1 + m0),-2.0) * (m1/r0));
+  double v1 = -std::sqrt(m0*m0 * std::pow((m1 + m0),-2.0) * (m1/r0));
 
   // Initial conditions
   std::vector<double> x_init;
   x_init.assign(DOFs,0.0);
-  x_init[0] = r0;
   std::vector<double> v_init;
   v_init.assign(DOFs,0.0);
-  v_init[1] = v0;
 
   // Set up parameters for large planet
+  x_init[0] = r0;
+  v_init[1] = v0;
   PlanetSystem.ElementList[0].set_mass(m0);
   PlanetSystem.ElementList[0].set_Coordinates(x_init);
   PlanetSystem.ElementList[0].set_Velocities(v_init);
 
   // Set up parameters for small planet
-  PlanetSystem.ElementList[1].set_mass(1.0*m0);
-  PlanetSystem.ElementList[1].set_Coordinates((-1.0) * x_init);
-  PlanetSystem.ElementList[1].set_Velocities((-1.0) * v_init);
+  x_init[0] = r1;
+  v_init[1] = v1;
+  PlanetSystem.ElementList[1].set_mass(m1);
+  PlanetSystem.ElementList[1].set_Coordinates(x_init);
+  PlanetSystem.ElementList[1].set_Velocities(v_init);
 
   // Perform Simulation
-  PlanetSystem.Evolve(0.0,1*M_PI*r0/v0,0.0001);
+  PlanetSystem.Evolve(0.0,2.0*M_PI*r0/v0,0.0001);
 
   // Return memory
   PlanetSystem.KillSystem();
