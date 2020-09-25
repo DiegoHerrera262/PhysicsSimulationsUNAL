d0811218ffb8f96b170dc22961ae3a672e54cd57
DiegoHerrera262 <diegoherrera262@gmail.com>
2020-09-25 10:00:11 -0500

    Data & Graphics for Task 1A.

--- ClassWork/RK4/SIR_Graphics_A.m
+++ ClassWork/RK4/SIR_Graphics_A.m
@@ -6,8 +6,8 @@
 clear all; close all; clc;
 
 %% Datafile name
-InputDataFile = 'DataFiles/Punto1A.txt';
-OutputDataFile = 'Figures/Punto1A.pdf';
+InputDataFile = 'DataFiles/Task1A.txt';
+OutputDataFile = 'Figures/Task1A.pdf';
 
 %% Read data from file
 SIR_Data = readtable(InputDataFile);
@@ -22,8 +22,16 @@ plot(data(:,1),data(:,3),'LineWidth',2.0,'Color','r'); hold on;
 % Plot recovered
 plot(data(:,1),data(:,4),'LineWidth',2.0,'Color','k');
 %% Control aspect of graphics
-xlabel('Time','FontSize',19);
-ylabel('Proportion of SIR','FontSize',19);
-title('Integration of SIR Model using RK4','FontSize',19);
+xlabel('Time','FontSize',19,'interpreter','latex');
+ylabel('Proportion of SIR','FontSize',19,'interpreter','latex');
+title('Integration of SIR Model using RK4', '$\beta=0.35$ and $\gamma=0.08$',...
+      'FontSize',19,'interpreter','latex');
+%% Inlude text labels
+text(55,0.2,'Inf.','FontSize',15,'Color','r','interpreter','latex');
+text(21,0.9,'Susc.','FontSize',15,'Color','b','interpreter','latex');
+text(40,0.85,'Recov.','FontSize',15,'Color','k','interpreter','latex');
 %% Save figure
-exportgraphics(myplot,OutputDataFile,'ContentType','vector');
+tag = input('Is the plot Proper to be saved? (1/0): ');
+if tag == 1
+  exportgraphics(myplot,OutputDataFile,'ContentType','vector');
+end
